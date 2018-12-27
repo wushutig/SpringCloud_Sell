aa2067fbbfde5aa6f253bdd6542ab048144036dd
wushuting <614056226@qq.com>
2018-12-27 19:57:14 +0800

    config

---
+++ configgit/config/src/main/java/com/imooc/config/ConfigApplication.java
@@ -0,0 +1,20 @@
+package com.imooc.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+@RefreshScope
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+}
+
